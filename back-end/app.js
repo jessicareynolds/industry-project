721b3d10aec1bfb21d710fe5fccabbd1ddf7bdfa
Ziedah Ward <ziedahward@Ziedahs-MacBook-Pro.local>
2020-09-17 19:59:03 -0400

    Update app.js and complete back-end

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const port = 5000 || process.env.PORT
 const productsRouter = require("./routes/products-routes")
+const storesRouter = require("./routes/stores-routes")
 const mongoose = require("mongoose")
 const mongoUrl = "mongodb://mlab2020:abc123def!@ds031617.mlab.com:31617/learningmongo"
 
@@ -12,9 +13,14 @@ connection.once("open", () => {
   console.log("Mongodb database connection established successfully");
 })
 
-
-app.use("/",productsRouter)
+//set up additional  fields
 app.use(express.json())
+app.use("/",productsRouter)
+app.use("/stores",storesRouter)
+// another way to importing a route
+app.use("/categories",require("./routes/categories-routes"))
+
+
 
 app.listen(port,()=>{
     console.log(`server running on port : ${port}`)
--- back-end/models/stores.model.js
+++ back-end/models/stores.model.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
 
-const storesSchema = new Schema({
+const storeSchema = new Schema({
     logo: String,
     name: String,
     description: String
