72c4e2209cef6ea603261c9fcf1f378cb0ae5518
jessicareynolds <reynolds.jessann@gmail.com>
2020-09-22 21:08:35 -0400

    Adds async function

--- back-end/index.js
+++ back-end/index.js
@@ -7,6 +7,21 @@ const mongoose = require("mongoose")
 const mongoUrl = "mongodb+srv://admin:RURVoDxjkFsQto7A@cluster0.0apnz.mongodb.net/IndustryProject"
 const cors = require("cors")
 
+const {MongoClient} = require('mongodb');
+
+let database = null;
+
+async function startDatabase() {
+  const mongoDBURL = "mongodb+srv://admin:RURVoDxjkFsQto7A@cluster0.0apnz.mongodb.net/IndustryProject";
+  const connection = await MongoClient.connect(mongoDBURL, {useNewUrlParser: true});
+  database = connection.db();
+}
+
+async function getDatabase() {
+  if (!database) await startDatabase();
+  return database;
+}
+
 // connects mongoose to database
 mongoose.connect(mongoUrl,{ useNewUrlParser: true, useCreateIndex: true , useUnifiedTopology: true});
 const connection = mongoose.connection;
@@ -18,11 +33,16 @@ console.log("Mongodb database connection established successfully");
 app.use(express.json())
 app.use(cors)
 app.use("/",productsRouter)
-app.use("/stores",storesRouter)
+app.use("/Stores",storesRouter)
 // another way to importing a route
-app.use("/categories",require("./routes/categories-routes"))
+app.use("/Categories",require("./routes/categories-routes"))
 
 
 app.listen(port,()=>{
     console.log(`server running on port : ${port}`)
 })
+
+module.exports = {
+    getDatabase,
+    startDatabase,
+  };
